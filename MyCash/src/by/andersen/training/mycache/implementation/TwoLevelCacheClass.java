fb8a57f83d93710de4b5fb3558019c17ead6914b
Alexander Kazimov <bybirmybir@gmail.com>
2019-02-01 11:39:25 +0300

    Create 2lvl cash. Create implementation interfaces.

---
+++ MyCash/src/by/andersen/training/mycache/implementation/TwoLevelCacheClass.java
@@ -0,0 +1,159 @@
+package by.andersen.training.mycache.implementation;
+
+import by.andersen.training.mycache.interfaces.ILeveledCache;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TwoLevelCacheClass<KeyType,ValueType extends Serializable> implements ILeveledCache<KeyType,ValueType> {
+
+    RamCacheClass<KeyType,ValueType> ramCache;
+    MemoryCacheClass<KeyType,ValueType> memoryCache;
+    int maxRamCacheCapacity;
+    int numberOfRequests;
+    int numberOfRequestsForRecahce;
+
+    public TwoLevelCacheClass(int _maxRamCacheCapacity, int _numberOfRequestsForRecache)
+    {
+        maxRamCacheCapacity = _maxRamCacheCapacity;
+        numberOfRequestsForRecahce = _numberOfRequestsForRecache;
+        ramCache = new RamCacheClass<KeyType, ValueType>();
+        memoryCache = new MemoryCacheClass<KeyType, ValueType>();
+        numberOfRequests = 0;
+    }
+
+
+    @Override
+    public void recache() throws IOException, ClassNotFoundException {
+        TreeSet<KeyType> ramKeySet = new TreeSet<KeyType>(ramCache.getMostFrequentlyUsedKeys());
+        int boundFrecquency = 0;
+
+
+        // вычисление среднего арифметического для отбрасывания редко вызываемых объектов
+        for(KeyType key: ramKeySet)
+        {
+            boundFrecquency += ramCache.getFrecquencyOfCallingObject(key);
+        }
+        boundFrecquency /= ramKeySet.size();
+
+
+        for(KeyType key: ramKeySet)
+        {
+            if(ramCache.getFrecquencyOfCallingObject(key) <= boundFrecquency){
+                memoryCache.cache(key,ramCache.removeObject(key));
+            }
+        }
+
+        TreeSet<KeyType> memoryKeySet = new TreeSet<KeyType>(memoryCache.getMostFrequentlyUsedKeys());
+        for(KeyType key : memoryKeySet)
+        {
+            try{
+                if(memoryCache.getFrecquencyOfCallingObject(key) > boundFrecquency)
+                {
+                    ramCache.cache(key,memoryCache.removeObject(key));
+                }
+            }
+            catch (IOException ex)
+            {
+                memoryCache.deleteObject(key);
+                continue;
+            }
+            catch (ClassNotFoundException ex)
+            {
+                ex.printStackTrace();
+                continue;
+            }
+        }
+    }
+
+    @Override
+    public void cache(KeyType keyType, ValueType valueType) throws IOException, ClassNotFoundException {
+        ramCache.cache(keyType,valueType);
+    }
+
+    @Override
+    public ValueType getObject(KeyType keyType) throws IOException, ClassNotFoundException {
+        if(ramCache.containsKey(keyType)){
+            numberOfRequests++;
+            if(numberOfRequests > numberOfRequestsForRecahce){
+                this.recache();
+                numberOfRequests = 0;
+            }
+            return ramCache.getObject(keyType);
+        }
+        if(memoryCache.containsKey(keyType)){
+            numberOfRequests++;
+            if(numberOfRequests > numberOfRequestsForRecahce){
+                this.recache();
+                numberOfRequests = 0;
+            }
+            return  memoryCache.getObject(keyType);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteObject(KeyType keyType) {
+        if(ramCache.containsKey(keyType)){
+            ramCache.deleteObject(keyType);
+        }
+        if(memoryCache.containsKey(keyType)){
+            memoryCache.deleteObject(keyType);
+        }
+    }
+
+    @Override
+    public void clearCache() {
+        memoryCache.clearCache();
+        ramCache.clearCache();
+    }
+
+    @Override
+    public ValueType removeObject(KeyType keyType) {
+        if(ramCache.containsKey(keyType)){
+            return ramCache.removeObject(keyType);
+        }
+        if(memoryCache.containsKey(keyType)){
+            return  memoryCache.removeObject(keyType);
+        }
+        return null;
+    }
+
+    @Override
+    public boolean containsKey(KeyType keyType) {
+        if(ramCache.containsKey(keyType)){
+            return true;
+        }
+        if(memoryCache.containsKey(keyType)){
+            return  true;
+        }
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return ramCache.size() + memoryCache.size();
+    }
+
+    @Override
+    public Set<KeyType> getMostFrequentlyUsedKeys() {
+        TreeSet<KeyType> set = new TreeSet<KeyType>(ramCache.getMostFrequentlyUsedKeys());
+        set.addAll(memoryCache.getMostFrequentlyUsedKeys());
+        return set;
+    }
+
+    @Override
+    public int getFrecquencyOfCallingObject(KeyType key) {
+        if(ramCache.containsKey(key))
+        {
+            return ramCache.getFrecquencyOfCallingObject(key);
+        }
+        if(memoryCache.containsKey(key))
+        {
+            return memoryCache.getFrecquencyOfCallingObject(key);
+        }
+        return 0;
+    }
+}
--- MyCash/src/by/andersen/training/mycache/interfaces/ICache.java
+++ MyCash/src/by/andersen/training/mycache/interfaces/ICache.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public interface ICache<KeyType,ValueType> {
 
     void cache(KeyType keyType, ValueType valueType) throws IOException,ClassNotFoundException;
-    ValueType getObject(KeyType keyType);
+    ValueType getObject(KeyType keyType) throws IOException, ClassNotFoundException;
     void deleteObject(KeyType keyType);
     void clearCache();
     ValueType removeObject(KeyType keyType);
